63ffca9ba45e18ba8b289a6e363d6e1368db8597
matsuyamayusaku <s21c1114if@s.chibakoudai.jp>
2022-12-14 15:07:48 +0900

    bug fix

--- hand.py
+++ hand.py
@@ -36,33 +36,33 @@ def main():
       image.flags.writeable = True
       image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
       if results.multi_hand_landmarks:
-        #必要な手の関節座標を変数に格納
-        for index, landmark in enumerate(hand_landmarks.landmark):
-          landmark_x = min(int(landmark.x * image_width), image_width - 1)
-          landmark_y = min(int(landmark.y * image_height), image_height - 1)
-          if index == 0:
-            cx0,cy0 = landmark_x, landmark_y
-          if index == 9:
-            cx9,cy9 = landmark_x, landmark_y
-        #取得した座標の中間点を表示
-        gap1x,gap1y = (cx0+cx9)/2, (cy0+cy9)/2
-        gap1x = int(gap1x)
-        gap1y = int(gap1y)
-        cv2.circle(image, (gap1x,gap1y), 5, (0, 255, 0), 2)
-        gap1x,gap1y = gap1x-image_width/2,-(gap1y-image_height/2)
-
-        array_points.points = []
-        point_1 = Point()
-        point_1.x = gap1x/3000
-        point_1.y = gap1y/3000
-        point_1.z = 0
-        array_points.points.append(point_1)
-        hand_position.publish(array_points)
-        #カメラに表示
-        cv2.putText(image,"gap1x:"+str(gap1x)+"gap1y:"+str(gap1y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-
-
         for hand_landmarks in results.multi_hand_landmarks:
+          #必要な手の関節座標を変数に格納
+          for index, landmark in enumerate(hand_landmarks.landmark):
+            landmark_x = min(int(landmark.x * image_width), image_width - 1)
+            landmark_y = min(int(landmark.y * image_height), image_height - 1)
+            if index == 0:
+              cx0,cy0 = landmark_x, landmark_y
+            if index == 9:
+              cx9,cy9 = landmark_x, landmark_y
+          #取得した座標の中間点を表示
+          gap1x,gap1y = (cx0+cx9)/2, (cy0+cy9)/2
+          gap1x = int(gap1x)
+          gap1y = int(gap1y)
+          cv2.circle(image, (gap1x,gap1y), 5, (0, 255, 0), 2)
+          gap1x,gap1y = gap1x-image_width/2,-(gap1y-image_height/2)
+
+          array_points.points = []
+          point_1 = Point()
+          point_1.x = gap1x/3000
+          point_1.y = gap1y/3000
+          point_1.z = 0
+          array_points.points.append(point_1)
+          hand_position.publish(array_points)
+          #カメラに表示
+          cv2.putText(image,"gap1x:"+str(gap1x)+"gap1y:"+str(gap1y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
+
           mp_drawing.draw_landmarks(
               image,
               hand_landmarks,
