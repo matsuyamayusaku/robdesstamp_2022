1fb5fc601ecd6cf39b07c4e4ee0f99fb99dfdddc
matsuyamayusaku <s21c1114if@s.chibakoudai.jp>
2022-12-17 16:33:04 +0900

    don't know if the program works

--- hand.py
+++ hand.py
@@ -11,74 +11,90 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 from std_msgs.msg import Float32
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-def main():
-  rospy.init_node("MediaPipe_node")
-  hand_position_x = rospy.Publisher('hand_topic_x', Float32, queue_size=10)
-  hand_position_y = rospy.Publisher('hand_topic_y', Float32, queue_size=10)
-  # For webcam input:
-  #cap = cv2.VideoCapture(0)
-  cap = rospy.Subscriber("/camera/color/image_info", Image, self.color_callback_and_convert)
-  with mp_hands.Hands(
-      model_complexity=0,
-      min_detection_confidence=0.5,
-      min_tracking_confidence=0.5) as hands:
-    while not rospy.is_shutdown():
-      success, image = cap.read()
-      if not success:
-        print("Ignoring empty camera frame.")
-        # If loading a video, use 'break' instead of 'continue'.
-        continue
+class Home(object):
+  def __init__(self):
+    rospy.init_node("MediaPipe_node")
+    self.bridge = CvBridge()
+    self.subscribed_image = rospy.Subscriber("/camera/color/image_info", Image, self.callback)
+    self.hand_position_x = rospy.Publisher('hand_topic_x', Float32, queue_size=10)
+    self.hand_position_y = rospy.Publisher('hand_topic_y', Float32, queue_size=10)
 
-      # To improve performance, optionally mark the image as not writeable to
-      # pass by reference.
-      image.flags.writeable = False
-      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-      results = hands.process(image)
+  def main(self):
+    try:
+      rospy.spin()
+    except KeyboardInterrupt:
+      print("daaa!")
+      cv2.destroyAllWindows()
 
-      # Draw the hand annotations on the image.
-      image.flags.writeable = True
-      image_height, image_width, _ = image.shape
-      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-      if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-          #必要な手の関節座標を変数に格納
-          for index, landmark in enumerate(hand_landmarks.landmark):
-            landmark_x = min(int(landmark.x * image_width), image_width - 1)
-            landmark_y = min(int(landmark.y * image_height), image_height - 1)
-            if index == 0:
-              cx0,cy0 = landmark_x, landmark_y
-            if index == 9:
-              cx9,cy9 = landmark_x, landmark_y
-          #取得した座標の中間点を表示
-          gap1x,gap1y = (cx0+cx9)/2, (cy0+cy9)/2
-          gap1x = int(gap1x)
-          gap1y = int(gap1y)
-          cv2.circle(image, (gap1x,gap1y), 5, (0, 255, 0), 2)
-          gap1x,gap1y = gap1x-image_width/2,-(gap1y-image_height/2)
+  def callback(self, msg):
+    try:
+      self.cap = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError as err:
+      print(err)
+    
+    #cap = cv2.VideoCapture(0)
+    with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+      while not rospy.is_shutdown():
+        success, image = self.cap.read()
+        if not success:
+          print("Ignoring empty camera frame.")
+          # If loading a video, use 'break' instead of 'continue'.
+          continue
 
-          point_x = gap1x/3000
-          point_y = gap1y/3000
-          hand_position_x.publish(point_x)
-          hand_position_y.publish(point_y)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
 
-          mp_drawing.draw_landmarks(
-              image,
-              hand_landmarks,
-              mp_hands.HAND_CONNECTIONS,
-              mp_drawing_styles.get_default_hand_landmarks_style(),
-              mp_drawing_styles.get_default_hand_connections_style())
-      # Flip the image horizontally for a selfie-view display.
-      cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-      
-      #カメラに座標表示
-      cv2.putText(image,"gap1x:"+str(gap1x)+"gap1y:"+str(gap1y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image_height, image_width, _ = image.shape
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+          for hand_landmarks in results.multi_hand_landmarks:
+            #必要な手の関節座標を変数に格納
+            for index, landmark in enumerate(hand_landmarks.landmark):
+              landmark_x = min(int(landmark.x * image_width), image_width - 1)
+              landmark_y = min(int(landmark.y * image_height), image_height - 1)
+              if index == 0:
+                cx0,cy0 = landmark_x, landmark_y
+              if index == 9:
+                cx9,cy9 = landmark_x, landmark_y
+            #取得した座標の中間点を表示
+            gap1x,gap1y = (cx0+cx9)/2, (cy0+cy9)/2
+            gap1x = int(gap1x)
+            gap1y = int(gap1y)
+            cv2.circle(image, (gap1x,gap1y), 5, (0, 255, 0), 2)
+            gap1x,gap1y = gap1x-image_width/2,-(gap1y-image_height/2)
 
-      if cv2.waitKey(5) & 0xFF == 27:
-        break
+            point_x = gap1x/3000
+            point_y = gap1y/3000
+            self.hand_position_x.publish(point_x)
+            self.hand_position_y.publish(point_y)
+
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+        # Flip the image horizontally for a selfie-view display.
+        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+
+        #カメラに座標表示
+        cv2.putText(image,"gap1x:"+str(gap1x)+"gap1y:"+str(gap1y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
+        if cv2.waitKey(5) & 0xFF == 27:
+          break
       
 if __name__ == '__main__':
   try:
-    main()
+    Home().main()
   except rospy.ROSInterruptException: 
     pass  
--- main_motion.py
+++ main_motion.py
@@ -23,213 +23,221 @@ from tf.transformations import quaternion_from_euler
 
 import random
 
-def main():
-    rospy.init_node("motion")
-    robot = moveit_commander.RobotCommander()
-    arm = moveit_commander.MoveGroupCommander("arm")
-    arm.set_max_velocity_scaling_factor(0.12)
-    arm.set_max_acceleration_scaling_factor(0.5)
-    gripper = moveit_commander.MoveGroupCommander("gripper")
+class Home(object):
 
-    while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
+    def __init__(self):
+        rospy.init_node("motion")
+        rospy.Subscriber('/hand_topic_x', Float32, self.callback, queue_size=1)
+        rospy.Subscriber('/hand_topic_y', Float32, self.callback, queue_size=1)
+        self.robot = moveit_commander.RobotCommander()
+
+
+    def motion(self):
+        arm = moveit_commander.MoveGroupCommander("arm")
+        arm.set_max_velocity_scaling_factor(0.12)
+        arm.set_max_acceleration_scaling_factor(0.5)
+        gripper = moveit_commander.MoveGroupCommander("gripper")
+
+        while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
+            rospy.sleep(1.0)
         rospy.sleep(1.0)
-    rospy.sleep(1.0)
 
-    print("Group names:")
-    print(robot.get_group_names())
+        print("Group names:")
+        print(robot.get_group_names())
 
-    print("Current state:")
-    print(robot.get_current_state())
+        print("Current state:")
+        print(robot.get_current_state())
 
-    # アーム初期ポーズを表示
-    arm_initial_pose = arm.get_current_pose().pose
-    print("Arm initial pose:")
-    print(arm_initial_pose)
+        # アーム初期ポーズを表示
+        arm_initial_pose = arm.get_current_pose().pose
+        print("Arm initial pose:")
+        print(arm_initial_pose)
 
-    # 何かを掴んでいた時のためにハンドを開く
-    gripper.set_joint_value_target([0.9, 0.9])
-    gripper.go()
-
-    # SRDFに定義されている"home"の姿勢にする
-    arm.set_named_target("home")
-    arm.go()
-    gripper.set_joint_value_target([0.7, 0.7])
-    gripper.go()
-
-    # 顔を認識するふり（ハンドを動かす）
-    for gupa in range(3):
+        # 何かを掴んでいた時のためにハンドを開く
         gripper.set_joint_value_target([0.9, 0.9])
         gripper.go()
-        rospy.sleep(0.7)
-        gripper.set_joint_value_target([0.3, 0.3])
+
+        # SRDFに定義されている"home"の姿勢にする
+        arm.set_named_target("home")
+        arm.go()
+        gripper.set_joint_value_target([0.7, 0.7])
         gripper.go()
-        rospy.sleep(0.7)
+
+        # 顔を認識するふり（ハンドを動かす）
+        for gupa in range(3):
+            gripper.set_joint_value_target([0.9, 0.9])
+            gripper.go()
+            rospy.sleep(0.7)
+            gripper.set_joint_value_target([0.3, 0.3])
+            gripper.go()
+            rospy.sleep(0.7)
 
 
 
 
-    #確率で左右どちらかを決める
-    ran = random.randint(1, 2)
-    if ran == 1:
-        pos_y = 0.2
-    else:
-        pos_y = -0.2
+        #確率で左右どちらかを決める
+        ran = random.randint(1, 2)
+        if ran == 1:
+            pos_y = 0.2
+        else:
+            pos_y = -0.2
 
 
 
 
 
-    # 掴む準備をする
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.2
-    target_pose.position.y = pos_y
-    target_pose.position.z = 0.3
-    q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
+        # 掴む準備をする
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = 0.2
+        target_pose.position.y = pos_y
+        target_pose.position.z = 0.3
+        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()  # 実行
 
-    # ハンドを開く
-    gripper.set_joint_value_target([0.7, 0.7])
-    gripper.go()
+        # ハンドを開く
+        gripper.set_joint_value_target([0.7, 0.7])
+        gripper.go()
 
-    # 掴みに行く
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.2
-    target_pose.position.y = pos_y
-    target_pose.position.z = 0.10
-    q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
+        # 掴みに行く
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = 0.2
+        target_pose.position.y = pos_y
+        target_pose.position.z = 0.10
+        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()  # 実行
 
-    # ハンドを閉じる
-    gripper.set_joint_value_target([0.4, 0.4])
-    gripper.go()
+        # ハンドを閉じる
+        gripper.set_joint_value_target([0.4, 0.4])
+        gripper.go()
 
-    # 持ち上げる
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.2
-    target_pose.position.y = pos_y
-    target_pose.position.z = 0.3
-    q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    print('I got a stamp!')
-    arm.go()							# 実行
+        # 持ち上げる
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = 0.2
+        target_pose.position.y = pos_y
+        target_pose.position.z = 0.3
+        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        print('I got a stamp!')
+        arm.go()							# 実行
 
-    # 移動する
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.3
-    target_pose.position.y = 0.0
-    target_pose.position.z = 0.3
-    q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
-    print("I'll stamp!!")
+        # 移動する
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = 0.3
+        target_pose.position.y = 0.0
+        target_pose.position.z = 0.3
+        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()  # 実行
+        print("I'll stamp!!")
 
-    # 下ろす
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.43
-    target_pose.position.y = 0.0
-    target_pose.position.z = 0.10
-    q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
-    print('pon')
-    rospy.sleep(1.0)
+        # 下ろす
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = 0.43
+        target_pose.position.y = 0.0
+        target_pose.position.z = 0.10
+        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()  # 実行
+        print('pon')
+        rospy.sleep(1.0)
 
-    # 少しだけハンドを持ち上げる
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.3
-    target_pose.position.y = 0.0
-    target_pose.position.z = 0.2
-    q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
+        # 少しだけハンドを持ち上げる
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = 0.3
+        target_pose.position.y = 0.0
+        target_pose.position.z = 0.2
+        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()  # 実行
 
-    # SRDFに定義されている"home"の姿勢にする
-    # どんなスタンプを押したのか見せつける
-    arm.set_named_target("home")
-    arm.go()
-    print('Ta-da!!!!')
-    rospy.sleep(2.0)
-
-
-    # スタンプを元の位置に戻す
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.15
-    target_pose.position.y = pos_y
-    target_pose.position.z = 0.10
-    q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
-
-    # ハンドを開く
-    gripper.set_joint_value_target([0.7, 0.7])
-    gripper.go()
-
-    # 上げる
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.15
-    target_pose.position.y = pos_y
-    target_pose.position.z = 0.3
-    q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()							# 実行
+        # SRDFに定義されている"home"の姿勢にする
+        # どんなスタンプを押したのか見せつける
+        arm.set_named_target("home")
+        arm.go()
+        print('Ta-da!!!!')
+        rospy.sleep(2.0)
 
 
-    # SRDFに定義されている"home"の姿勢にする
-    arm.set_named_target("home")
-    arm.go()
+        # スタンプを元の位置に戻す
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = 0.15
+        target_pose.position.y = pos_y
+        target_pose.position.z = 0.10
+        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()  # 実行
+
+        # ハンドを開く
+        gripper.set_joint_value_target([0.7, 0.7])
+        gripper.go()
+
+        # 上げる
+        target_pose = geometry_msgs.msg.Pose()
+        target_pose.position.x = 0.15
+        target_pose.position.y = pos_y
+        target_pose.position.z = 0.3
+        q = quaternion_from_euler(-3.14, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()							# 実行
+
+
+        # SRDFに定義されている"home"の姿勢にする
+        arm.set_named_target("home")
+        arm.go()
 
 
 
-    print("done")
+        print("done")
+
+    def run(self):
+        try:
+            while not rospy.is_shutdown():
+                #enterが押されるまで待機
+                try:
+                    input('\n\033[33m__STANDBY__\npress"enter"\033[0m\n')
+                    print('START')
+                except:
+                    print('START')
+                
+                self.motion()
+        except rospy.ROSInterruptException:
+            pass
 
 
 if __name__ == '__main__':
-
-    try:
-        # ↓ここifにすると1回だけ動いて止まる
-        while not rospy.is_shutdown():
-
-            #enterが押されるまで待機
-            try:
-                input('\n\033[33m__STANDBY__\npress"enter"\033[0m\n')
-                print('START')
-            except:
-                print('START')
-
-            main()
-    except rospy.ROSInterruptException:
-        pass
+    while not rospy.is_shutdown():
+        Home().run()
