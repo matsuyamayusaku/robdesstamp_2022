b7ac84e7f7a3429d3e9730d32da1d542d5631b74
matsuyamayusaku <s21c1114if@s.chibakoudai.jp>
2022-12-17 20:16:52 +0900

    bug fix

--- hand.py
+++ hand.py
@@ -6,6 +6,7 @@ import mediapipe as mp
 import rospy
 import math
 import sys
+from numpy.lib.type_check import imag
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
