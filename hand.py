87eb0fe160cc0bc88946860d3a05020a1fbab40b
matsuyamayusaku <s21c1114if@s.chibakoudai.jp>
2022-12-17 21:26:17 +0900

    research

--- hand.py
+++ hand.py
@@ -18,7 +18,7 @@ def main():
   hand_position_x = rospy.Publisher('hand_topic_x', Float32, queue_size=10)
   hand_position_y = rospy.Publisher('hand_topic_y', Float32, queue_size=10)
 
-  #カメラの認識番号に合わせる必要があります。
+  #カメラの認識番号に合わせる必要があります
   cap = cv2.VideoCapture(4)
   with mp_hands.Hands(
       model_complexity=0,
